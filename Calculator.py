19a837627a1c6417d95d2cbc0340dd032a98b3ec
Anjali <anjalijanar@gmail.com>
2023-07-22 11:30:38 +0530

    Write a program that performs the tasks of a simple calculator. The program should first take an integer as input and then based on that integer perform the task as given below. 1. If the input is 1, then 2 integers are taken from the user and their sum is printed. 2. If the input is 2, then 2 integers are taken from the user and their difference(1st number - 2nd number) is printed. 3. If the input is 3, then 2 integers are taken from the user and their product is printed. 4. If the input is 4, then 2 integers are taken from the user and the quotient obtained (on dividing 1st number by 2nd number) is printed. 5. If the input is 5, then 2 integers are taken from the user and their remainder(1st number mod 2nd number) is printed. 6. If the input is 6, then the program exits. 7. For any other input, then print invalid operation.

---
+++ Calculator.py
@@ -0,0 +1,42 @@
+while True:
+    n = int(input())
+    if n==1:
+        a = int(input())
+        b = int(input())
+        sum = a+b
+        print(sum)
+
+    elif n==2:
+        a = int(input()) 
+        b = int(input())
+        difference = a-b
+        print(difference)
+
+    elif n==3:
+        a = int(input()) 
+        b = int(input())
+        product = a*b
+        print(product)
+
+    elif n==4:
+        a = int(input()) 
+        b = int(input())
+        quotient = a//b
+        print(quotient)
+
+    elif n==5:
+        a = int(input()) 
+        b = int(input())
+        mod = a%b
+        print(mod)
+
+    elif n==6:
+        break  
+
+    else:
+        print("Invalid Operation")
+
+                 
+   
+   
+
