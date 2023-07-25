776d8ac05af0639b302954c7af6a47cee90c3d4c
Anjali <anjalijanar@gmail.com>
2023-07-25 11:20:58 +0530

    Print the following pattern for the given N number of rows. Pattern for N = 4
       1
      12
     123
    1234

---
+++ MirrorNumberPattern.py
@@ -0,0 +1,14 @@
+n = int(input())
+i=1
+while i<=n:
+    spaces = 1
+    while spaces<=n-i:
+        print(" ",end='')
+        spaces=spaces+1
+    j=1    
+    while j<=i:
+        print(j,end='')
+        j=j+1
+
+    print()
+    i=i+1
