f30b6353e1d26b0c2f1d610a5fdeefbee6aa49e8
Anjali <anjalijanar@gmail.com>
2023-08-06 22:45:28 +0530

    Count the numbers of vowels,consonants,digits and special charcters in a given string

---
+++ CountVowelsConsonantsDigitsAndSpecialCharactersInString.py
@@ -0,0 +1,17 @@
+def countInString(str):
+    v,c,d,s = 0,0,0,0
+    for char in str:
+        if(char>='a' and char<='z') or (char>='A' and char<='Z'):
+            char = char.lower()
+            if(char=='a' or char=='e' or char=='i' or char=='o' or char=='u'):
+                v+=1
+            else:
+                c+=1
+        elif(char>='0' and char<='9'):
+            d+=1
+        else:
+            s+=1
+    return v,c,d,s 
+str = 'cadvsfbgfhngjmykiSDWEV&^%$1232435'
+v,c,d,s = countInString(str)
+print(v,c,d,s)
